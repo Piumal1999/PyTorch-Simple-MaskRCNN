f3f9f54087c14db72fdc05d57f1900e18372d8b4
piumal1999 <piumalrathnayake@hotmail.com>
2024-05-12 02:52:06 +0530

    Try resnet101

--- pytorch_mask_rcnn/__init__.py
+++ pytorch_mask_rcnn/__init__.py
@@ -1,4 +1,4 @@
-from .model import maskrcnn_resnet50
+from .model import maskrcnn_resnet50, maskrcnn_resnet_fpn
 from .datasets import *
 from .engine import train_one_epoch, evaluate
 from .utils import *
